89aa44f3b938ce89e460f4148e2727f23d7aceab
Ardent Wave <ardentwave@outlook.com>
2018-08-19 03:35:48 -0500

    Standard assets and tracking camera

---
+++ 2d_ScratchPad/Assets/Scripts/TrackingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCamera : MonoBehaviour {
+
+    [SerializeField]
+    protected Transform trackingTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(trackingTarget.position.x, trackingTarget.position.y, transform.position.z);
+	}
+}
