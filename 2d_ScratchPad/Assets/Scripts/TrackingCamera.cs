f04ac379c70136f994164f5977adad6cbe1ed4ab
Ardent Wave <ardentwave@outlook.com>
2018-08-19 04:03:12 -0500

    Camera updates

--- 2d_ScratchPad/Assets/Scripts/TrackingCamera.cs
+++ 2d_ScratchPad/Assets/Scripts/TrackingCamera.cs
@@ -7,13 +7,29 @@ public class TrackingCamera : MonoBehaviour {
     [SerializeField]
     protected Transform trackingTarget;
 
+    [SerializeField]
+    float xOffset;
+
+    [SerializeField]
+    float yOffset;
+
+    [SerializeField]
+    float followSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(trackingTarget.position.x, trackingTarget.position.y, transform.position.z);
+	void Update ()
+    {
+        float xTarget = trackingTarget.position.x + xOffset;
+        float yTarget = trackingTarget.position.y + yOffset;
+
+        float xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * followSpeed);
+        float yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * followSpeed);
+
+        transform.position = new Vector3(xNew, yNew, transform.position.z);
 	}
 }
